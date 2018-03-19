d8a004da0e3469f01c7a60e1ad689c81ebeee34e
uplink007 <sight007@gmail.com>
2018-03-19 22:23:55 +0200

    os hw2: dir print

--- ex2.c
+++ ex2.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h> 
 #define INPUT_BUFFER 256
 typedef enum {false,true}bool;
 
@@ -25,6 +26,7 @@ void read_eval_print_loop(void){
 }
 
 void get_input_from_eval_loop(char* eval_loop_string){
-	 printf("OS SHell:");
+	char dir[INPUT_BUFFER];
+	 printf("OS SHell:%s",getcwd (dir, INPUT_BUFFER));
 	 fgets(eval_loop_string, INPUT_BUFFER, stdin);
 }
